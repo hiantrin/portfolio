c5f6284069998e9a72bcf3e4d740e785f6e33d6d
Hamza Iantrin <hiantrin@e2r4p9.1337.ma>
2022-11-29 20:20:07 +0100

    push with sweetalert

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,10 +10,12 @@ import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import emailjs from '@emailjs/browser';
+import swal from 'sweetalert';
 
 
 
 const Contact = () => {
+	const [errors, setErrors] = useState({})
     const myRef = useRef();
 	const infos = {
 		from_name: "",
@@ -25,18 +27,56 @@ const Contact = () => {
 	const [myElement, setMyElement] = useState(false);
 
 	
+	const validate = () => {
+		const error = {};
+		const regex = /^([a-zA-Z._0-9-]+)@([a-zA-Z0-9]+[.]?)*([a-zA-Z0-9])(\.[a-zA-Z]{2,4})$/i;
+		if (!userInfos.from_name)
+			error.from_name = "Name is required!"
+		if (!userInfos.email)
+			error.email = "Email is required!"
+		else if (!regex.test(userInfos.email) )
+			error.email = "This is not a valid email format!"
+		if (!userInfos.message)
+			error.message = "Please write your message"
+		if (Object.keys(error).length === 0) {
+			return false;
+		}
+		return(error);
+	}
+
 	const sendEmail = (e) => {
 		e.preventDefault();
-		setUserInfos(infos);
-		emailjs.send('service_1kds15t', 'template_rsxe4ek', {
-			...userInfos
-		}, 'bVMA5Waewx2Axvt2W')
-		  .then((result) => {
-			  console.log(result.text);
-		  }, (error) => {
-			  console.log(error.text);
-		  });
+		const error = validate()
+		if (error) setErrors(error)
+		else
+		{
+			setErrors({})
+			sendIt();
+		}
 	  };
+
+	
+	  const sendIt = () => {
+			setUserInfos(infos);
+			emailjs.send('service_1kds15t', 'template_rsxe4ek', {
+				...userInfos
+			}, 'bVMA5Waewx2Axvt2W')
+			.then((result) => {
+				swal({
+					title: "Yeeeeeeeeep!!",
+					text: "Your message has been sent succefully",
+					icon: "success",
+					buttons: "close"
+				})
+			}, (error) => {
+				swal({
+					title: "Ooooooops!!",
+					text: "Something Went Wrong Please Try Again!",
+					icon: "warning",
+					buttons: "close"
+				})
+			});
+	  }
 	
 
 	useEffect(() => {
@@ -67,19 +107,29 @@ const Contact = () => {
 		</div>
         <form className={show ? "flex flex-col items-center sm:items-start space-y-6 w-full sm:w-[600px] animate-message px-2 sm:px-0" : "flex flex-col space-y-6 w-full sm:w-[600px] animate-message2 px-2 sm:px-0"}  onSubmit={sendEmail}>
         	<div className="flex flex-col sm:flex-row space-y-6 sm:space-y-0 justify-between w-full ">
-				<div className="flex items-center space-x-4 px-4 py-2 border-2 border-sky-500 rounded-full w-full sm:w-[47%]">
-					<FontAwesomeIcon icon={faUser} className="text-white"/>
-					<input type="text" name="Name" placeholder='YOUR NAME' className='my-input' value={userInfos.from_name} onChange={(e) => setUserInfos({...userInfos, from_name: e.target.value})} />
+				<div className='flex flex-col w-full sm:w-[47%]'>
+					<div className="flex items-center space-x-4 px-4 py-2 border-2 border-sky-500 rounded-full w-full ">
+						<FontAwesomeIcon icon={faUser} className="text-white"/>
+						<input type="text" name="Name" placeholder='YOUR NAME' className='my-input' value={userInfos.from_name} onChange={(e) => setUserInfos({...userInfos, from_name: e.target.value})} />
+					</div>
+					<h1 className='text-sm text-red-600 ml-4 mt-1'>{errors.from_name}</h1>
 				</div>
-				<div className="flex items-center space-x-4 px-4 py-2 border-2 border-sky-500 rounded-full w-full sm:w-[47%]">
-					<FontAwesomeIcon icon={faEnvelope} className="text-white"/>
-					<input type="text" name="Email" placeholder='YOUR EMAIL' className='my-input' value={userInfos.email} onChange={(e) => setUserInfos({...userInfos, email: e.target.value})} />
+				<div className='flex flex-col w-full sm:w-[47%]'>
+					<div className="flex items-center space-x-4 px-4 py-2 border-2 border-sky-500 rounded-full w-full ">
+						<FontAwesomeIcon icon={faEnvelope} className="text-white"/>
+						<input type="text" name="Email" placeholder='YOUR EMAIL' className='my-input' value={userInfos.email} onChange={(e) => setUserInfos({...userInfos, email: e.target.value})} />
+					</div>
+					<h1 className='text-sm text-red-600 ml-4 mt-1'>{errors.email}</h1>
 				</div>
         	</div>
-			<div className="w-full h-28 flex space-x-4 px-4 py-2 border-2 border-sky-500 rounded-2xl overflow-y-hidden">
-				<FontAwesomeIcon icon={faMessage} className="text-white mt-1"/>
-				<textarea name="Email" placeholder='YOUR MESSAGE' className='my-input' value={userInfos.message} onChange={(e) => setUserInfos({...userInfos, message: e.target.value})}/>
+			<div className='flex flex-col w-full'>
+				<div className="w-full h-28 flex space-x-4 px-4 py-2 border-2 border-sky-500 rounded-2xl overflow-y-hidden">
+					<FontAwesomeIcon icon={faMessage} className="text-white mt-1"/>
+					<textarea name="Email" placeholder='YOUR MESSAGE' className='my-input' value={userInfos.message} onChange={(e) => setUserInfos({...userInfos, message: e.target.value})}/>
+				</div>
+				<h1 className='text-sm text-red-600 ml-4 mt-1'>{errors.message}</h1>
 			</div>
+			
 			<button className="w-max flex space-x-3 px-10 rounded-full text-white" type='submit'>
 				<FontAwesomeIcon icon={faPaperPlane} />
 				<h1 >SEND MESSAGE</h1>
