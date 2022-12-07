d0a4a1adbcc759a72d456757f95a7212aac8994d
hiantrin <hamzaiantrin1999@gmail.com>
2022-12-07 19:48:45 +0100

    change in aboutMe section

--- src/components/AbouMe.jsx
+++ src/components/AbouMe.jsx
@@ -51,7 +51,7 @@ const AbouMe = () => {
 			<div className='flex flex-col w-full  md:w-[50%] lg:w-[480px] mr-20 lg:mr-10 space-y-5'>
 				<h1 className='text-gray-500 text-xs xs:text-sm sm:text-lg'>Hello! My name is Hamza and I enjoy creating things that live on the internet.</h1>
 				<h1 className='text-gray-500 text-xs xs:text-sm sm:text-lg'>My interest in web development started back in 2019 When I decided to join 1337 Coding School, turns out that I am so good at programming. </h1>
-				<h1 className='text-gray-500 text-xs xs:text-sm sm:text-lg'>Now, after 3 years of studies at 1337 I became a Well-qualified Full stack Web Developer.</h1>
+				<h1 className='text-gray-500 text-xs xs:text-sm sm:text-lg'>Now, after 3 years of studying at 1337 I became a Well-qualified Full stack Web Developer.</h1>
 				<h1 className='text-gray-500 text-xs xs:text-sm sm:text-lg'>I have created multiple web application that you will see some of them in my portfolio.</h1>
 				<h1 className='text-gray-500 text-xs xs:text-sm sm:text-lg'>Also I have 6 months (internship) at DROPLEET between May 2021 - October 2021</h1>
 				<h1 className='text-gray-500 text-xs xs:text-sm sm:text-lg'>Here are some technologies that I am working with currently:</h1>
